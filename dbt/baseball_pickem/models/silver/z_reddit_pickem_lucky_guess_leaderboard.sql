5ba0417acdfaacf851499a99d49496baa17aaa88
mirandamanuel <49087932+mirandamanuel@users.noreply.github.com>
2025-03-11 15:18:08 -0700

    Update z_reddit_pickem_lucky_guess_leaderboard.sql

--- dbt/baseball_pickem/models/silver/z_reddit_pickem_lucky_guess_leaderboard.sql
+++ dbt/baseball_pickem/models/silver/z_reddit_pickem_lucky_guess_leaderboard.sql
@@ -1,24 +1,39 @@
 with init_clean as (
     select
         h as username,
-        k as guesses,
+        k as lucky_guesses,
         j,
         ROW_NUMBER() over () as row_num
     from {{ ref("stg_reddit_pickem_submissions") }}
-    where k !~ '\D'
 ),
 
-bound as (
-    select MIN(row_num) as lower_bound
+lower_bound as (
+    select row_num as lower_bound
     from init_clean
-    where j is not NULL
+    where
+        lucky_guesses = 'Lucky Guesses'
+),
+
+trim_lower_bound as (
+    select
+        username::text,
+        lucky_guesses::text,
+        ROW_NUMBER() over () as row_num
+    from init_clean
+    where
+        row_num > (select lower_bound.lower_bound from lower_bound)
+),
+
+upper_bound as (
+    select MIN(row_num) as upper_bound
+    from trim_lower_bound
+    where
+        username is NULL
 )
 
 select
     username::text,
-    guesses::int
-from init_clean
+    lucky_guesses::text
+from trim_lower_bound
 where
-    username is not NULL
-    and guesses is not NULL
-    and row_num > (select bound.lower_bound from bound)
+    row_num < (select upper_bound.upper_bound from upper_bound)
